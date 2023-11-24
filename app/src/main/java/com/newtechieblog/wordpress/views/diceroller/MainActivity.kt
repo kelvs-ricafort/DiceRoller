833600d38e20714fcced47daf5bb56b9ec55a16d
Kelvin Ricafort <kelvs.r@outlook.com>
2023-11-24 17:20:44 +0800

    Finished

--- app/src/main/java/com/newtechieblog/wordpress/views/diceroller/MainActivity.kt
+++ app/src/main/java/com/newtechieblog/wordpress/views/diceroller/MainActivity.kt
@@ -2,17 +2,52 @@ package com.newtechieblog.wordpress.views.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        /**
+         * This method is called when the activity is created.
+         */
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find the button in the layout
         val rollButton: Button = findViewById(R.id.btnRoll)
-        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-        toast.show()
 
+        // Set a click listener on the button to roll the dice when the user taps the button/
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.resultTextView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+/**
+ * Dice with a fixed number of sides.
+ */
+class Dice(private val numSides: Int) {
+
+    /**
+     * Do a random dice roll and return the results.
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
